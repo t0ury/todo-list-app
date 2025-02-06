f084fa17b0d0dda277758316d9bd708297f78f1f
Tuo Yuan <yuan@localhost.com>
2025-02-07 02:06:51 +0800

    feat/addTask: add new task to list with given details

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,24 +1,33 @@
 /** @format */
+import PropTypes from "prop-types";
 
-const Task = (props) => {
+const Task = ({ id, title, content, completeTask }) => {
   return (
-    <li
-      key=""
-      className="item flex flex-row justify-between gap-x-5 py-5">
+    <li className="item flex flex-row justify-between gap-x-5 py-5">
       <div className="flex min-w-0 gap-x-4 content-center align-middle">
         <h4 className="flex content-center align-middle border rounded-xl py-2  ">
-            task
-            </h4>
+          <span>Title</span>
+          <p className="m-2">{title}</p>
+        </h4>
         <div className="min-w-0 flex-auto  border rounded-xl">
-          <p className="m-2">
-            details...details...details...details...details...details...details...details...details...
-            details...details...details...details...details...details...details...details...details...
-          </p>
+          <span>Content</span>
+          <p className="m-2">{content}</p>
         </div>
       </div>
-      <button>Complete</button>
+      <button
+        aria-label="complete"
+        aria-hidden="true"
+        onClick={() => completeTask(id)}>
+        Complete
+      </button>
     </li>
   );
 };
-
+Task.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  completeTask: PropTypes.func.isRequired,
+  handle: PropTypes.func.isRequired,
+};
 export default Task;
--- src/components/Task/Tasks.jsx
+++ src/components/Task/Tasks.jsx
@@ -1,14 +1,119 @@
 /** @format */
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { AddTask, Task } from "./";
+import Ajv from "ajv";
+import ajvErrors from "ajv-errors";
 
-const Tasks = ({children}) => {
+const schema = {
+  type: "object",
+  properties: {
+    title: {
+      type: "string",
+      minLength: 2,
+      maxLength: 32,
+      errorMessage: {
+        type: "Tile must be string",
+        minLength: "Title must longer than 2 chars",
+        maxLength: "Title can not more than 32 chars",
+      },
+    },
+    content: {
+      type: "string",
+      minLength: 2,
+      maxLength: 140,
+      errorMessage: {
+        type: "Content must be string",
+        minLength: "Title must longer than 2 chars",
+        maxLength: "Title can not more than 140 chars",
+      },
+    },
+  },
+};
+const ajv = new Ajv({ allErrors: true });
+ajvErrors(ajv);
+const validateData = ajv.compile(schema);
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContent, setInputContent] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [errors, setErrors] = useState({});
+
+  const handleInputTitle = (e) => {
+    setInputTitle(e.target.value);
+  };
+  const handleInputContent = (e) => {
+    setInputContent(e.target.value);
+  };
+
+  const handleAdd = (event) => {
+    /** Start data validation */
+    event.preventDefault();
+    const isValid = validateData({ handleInputTitle, handleInputContent });
+    setErrors({});
+
+    if (!isValid) {
+      const fieldMessage = {};
+      validateData?.errors.forEach((errors) => {
+        const field = errors.instancePath.substring(1);
+        fieldMessage[field] = errors.message;
+      });
+      setErrors(fieldMessage);
+      return;
+    }
+    /**  End data validation */
+
+    setTasks((prevState) => [
+      ...prevState,
+      {
+        key: Date.now,
+        id: Date.now,
+        title: inputTitle,
+        content: inputContent,
+      },
+    ]);
+    setInputTitle("");
+    setInputContent("");
+  };
+  const completeTask = (id) => {
+    const filterList = tasks.filter((task) => task.id !== id);
+    setTasks(filterList);
+  };
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("toDoList"));
+    data && setTasks(data);
+    setIsLoading(false);
+  }, []);
+
   return (
-    <ul className="task-list flex flex-col divide-y-4 divide-gray-200">
-      {children}
-    </ul>
+    <>
+      <div className="list__header">
+        <AddTask
+          handleAdd={handleAdd}
+          inputTitle={inputTitle}
+          inputContent={inputContent}
+          errors={errors}
+          handleInputTitle={handleInputTitle}
+          handleInputContent={handleInputContent}
+        />
+      </div>
+      <div className="list__body">
+        <ul className="task-list flex flex-col divide-y-4 divide-gray-200">
+          {tasks.map((task) => (
+            <Task
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              content={task.content}
+              completeTask={completeTask}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
-Tasks.propTypes = {
-  children: PropTypes.element.isRequired, 
-};
+
 export default Tasks;
